cd80dafe2dbe3bb2d7f659444a1975e522b68f13
Moad Eltabu <moadeltabu@gmail.com>
2017-04-30 11:33:53 -0400

    Adding illuminate/database using composer

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,7 +2,16 @@
 
 class Controller
 {
-    
+    public function model($model)
+    {
+         require_once '../app/models/' . $model . '.php';
+         return new $model();
+    }
+
+    public function view($view, $data = [])
+    {
+        require_once '../app/views/' . $view . '.php';
+    }
 }
 
 ?>
